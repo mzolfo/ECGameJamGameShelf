ff85221c52a0b92ed63c88a441267cad9df899ff
mzolfo <32077843+mzolfo@users.noreply.github.com>
2019-05-31 18:42:37 -0500

    got grappling hook thing mostly working besides the rope in between

--- we got a rope/Assets/Scripts/PlayerMotion.cs
+++ we got a rope/Assets/Scripts/PlayerMotion.cs
@@ -10,6 +10,9 @@ public class PlayerMotion : MonoBehaviour
     private float movementSpeed;
     private string myPlayerNumName;
     private Rigidbody2D myRigidbody;
+    [SerializeField]
+    private RopeHeadBehavior myRopeHead;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +21,16 @@ public class PlayerMotion : MonoBehaviour
         myPlayerNumName = myPlayerNum.ToString();
     }
 
+    private void Update()
+    {
+        if (myRopeHead.myHeadState == RopeHeadState.Retracted)
+        {
+            if (Input.GetButtonDown(myPlayerNumName + "Fire"))
+            {
+                FireRope();
+            }
+        }
+    }
 
     private void FixedUpdate()
     {
@@ -39,5 +52,17 @@ public class PlayerMotion : MonoBehaviour
         Vector2 movementVector = new Vector2(horizontalInput * movementSpeed * Time.deltaTime, myRigidBody2d.velocity.y);
         myRigidBody2d.velocity = movementVector;
         */
+    }
+
+    private void FireRope()
+    {
+        myRopeHead.myHeadState = RopeHeadState.Extending;
+        //we want this one to fire a segmented rope in a line in the direction of the other player, what do we need.
+       //take it in steps: first we calculate which direction the rope will fire in
+       //then we send the end of it in that direction the end must be mobile and able to collide with things but ignore the player's movement to begin
+       //then upon collision the rope needs to check how it interacts with things.
+
+        //trouble: if the rope connects to something how do its segments know what sort of an offset they need to account their movement according to the attached player's
+        //what sort of interactions does this rope actually need?
     }
 }
---
+++ we got a rope/Assets/Scripts/RopeHeadBehavior.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RopeHeadState { Retracted, Extending, Attached, Retracting };
+public class RopeHeadBehavior : MonoBehaviour
+{
+    
+    public RopeHeadState myHeadState;
+    [SerializeField]
+    private GameObject Player1;
+    [SerializeField]
+    private GameObject Player2;
+    [SerializeField]
+    private int MyAssignedPlayer;
+    [SerializeField]
+    private float speed;
+    
+    private Transform myPlayer;
+    private Transform otherPlayer;
+    private LineRenderer myLineRenderer;
+    private BoxCollider2D myOwnCollider;
+
+    //this is the end of the rope each player fires and it needs a few rules for how it works.
+
+    //first, if its x or y position is offset from the player it is meant to be attached to by any amount it needs to rotate itself to
+    //point its back side at that player.
+
+    //second is that its motion relys on when/where it was fired from and whether it has collided with anything
+    //while moving through the air its motion vector should simply be away from whichever player fired it at a given frame.
+    //after colliding it should check to see what it collided with to see if it should remain stationary or detach/bounce off
+    //when retracting it should simply be directly at the player who fired it.
+
+    //button pressed, calculatedirectionofextension, begin moving in direction of calculated extension 
+    //and check if you've collided with anything other than your own player.
+
+    // Start is called before the first frame update
+    void Start()
+    {//do we need this to repeat as we enter new scenes?
+        myOwnCollider = GetComponent<BoxCollider2D>();
+        myLineRenderer = GetComponent<LineRenderer>();
+        if (MyAssignedPlayer == 1)
+        {
+            myPlayer = Player1.transform;
+            otherPlayer = Player2.transform;
+        }
+        else if (MyAssignedPlayer == 2)
+        {
+            myPlayer = Player2.transform;
+            otherPlayer = Player1.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myHeadState == RopeHeadState.Retracted)
+        {
+            RetractedMove();
+        }
+        else if (myHeadState == RopeHeadState.Extending)
+        {
+            ExtendingMove();
+        }
+        else if (myHeadState == RopeHeadState.Retracting)
+        {
+            RetractingMove();
+        }
+        else if (myHeadState == RopeHeadState.Attached)
+        {
+            AttachedMove();
+        }
+        myLineRenderer.SetPosition(0, transform.position);
+        myLineRenderer.SetPosition(1, myPlayer.position);
+    }
+
+    void RetractedMove()
+    {
+        this.transform.position = myPlayer.position;
+        this.transform.rotation = new Quaternion(0, 0, 0, 0);
+        //when retracted, stay attached at your player's origin point and do not divert from it, keep rotation at 0
+
+    }
+
+    void ExtendingMove()
+    {
+        CalculateDirectionOfExtension();
+        if (HasReachedOtherPlayer())
+        { //this normally will mean just finish the stage
+            myHeadState = RopeHeadState.Retracting;
+        }
+        else
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, otherPlayer.position, step);
+        }
+        //when extending move in the direction of the other player while always pointing your back to your own player.
+    }
+
+    void AttachedMove()
+    {
+        //deactivate your own head's sprite and collider, remain attached to the postion of the attached object. 
+    }
+
+    void RetractingMove()
+    {
+        if (this.transform.position == myPlayer.position)
+        {
+            myHeadState = RopeHeadState.Retracted;
+        }
+        else
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, myPlayer.position, step);
+        }
+        //move directly at your player, keep your back pointing directly at that point. collider should be inactive.
+    }
+
+    private bool HasReachedOtherPlayer()
+    {
+        if (this.transform.position == otherPlayer.position)
+        {
+            return true;
+        }
+        else { return false; }       
+    }
+    private Vector3 CalculateDirectionOfMyPlayer()
+    {
+        //this object.position - player.position
+        return this.transform.position - myPlayer.position;
+    }
+
+    private Vector3 CalculateDirectionOfExtension()
+    {
+        //myplayer.position - otherplayer.position
+        return this.transform.position - otherPlayer.position;
+    }
+    /*
+    float step = speed * Time.deltaTime;
+    transform.position = Vector3.MoveTowards(transform.position, aerialObjective, step);
+    */
+}
