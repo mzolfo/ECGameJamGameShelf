411fffeca44847c724713eab676679df8d56ad2f
mzolfo <32077843+mzolfo@users.noreply.github.com>
2019-06-02 23:37:18 -0500

    added animated whiteplate to levelscene

---
+++ we got a rope/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneManager : MonoBehaviour
+{
+    private int CurrentScene;
+    [SerializeField]
+    private AudioClip MenuMusic;
+    [SerializeField]
+    private AudioClip StartLevelMusic;
+    [SerializeField]
+    private AudioClip LoopLevelMusic;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ we got a rope/Assets/Scripts/WhitePlate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhitePlate : MonoBehaviour
+{
+    public bool fadedToWhite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        fadedToWhite = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Endframe()
+    {
+        fadedToWhite = true;
+    }
+}
